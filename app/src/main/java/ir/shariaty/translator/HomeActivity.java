810af80302c01f62d9838e69f9beab698fc35b93
ABEDI-S <abedi.b831@gmail.com>
2021-12-31 10:37:28 +0330

    Sixth Commit _ Home-JAVA Add Developer Name to Splash Screen Add ActionBar

--- app/src/main/java/ir/shariaty/translator/HomeActivity.java
+++ app/src/main/java/ir/shariaty/translator/HomeActivity.java
@@ -1,19 +1,33 @@
 package ir.shariaty.translator;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
+import android.view.translation.Translator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,10 +36,10 @@ public class HomeActivity extends AppCompatActivity {
     private Button translateBtn;
     private TextView translatedTV;
 
-    String[] toLanguages = {"English","Persian","Arabic"};
-    String[] fromLanguages = {"Persian","English","Arabic"};
+    String[] fromLanguages = {"From","English","Turkish","Persian","Arabic"};
+    String[] toLanguages = {"To","English","Turkish","Persian","Arabic"};
 
-    private  static final int REQUEST_PERMISSION_CODE = 1;
+    private static final int REQUEST_PERMISSION_CODE = 1;
     int languageCode,fromLanguageCode,toLanguageCode = 0;
 
     @Override
@@ -74,6 +88,62 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 translatedTV.setText("");
+                if(sourceEdt.getText().toString().isEmpty()){
+                    Toast.makeText(HomeActivity.this,"Please Enter Your Text To Translate.",Toast.LENGTH_SHORT).show();
+                }
+                else if(fromLanguageCode == 0){
+                    Toast.makeText(HomeActivity.this,"Please Select Source Language.",Toast.LENGTH_SHORT).show();
+                }
+                else if(toLanguageCode == 0){
+                    Toast.makeText(HomeActivity.this,"Please Select The Language To Make Translation.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    translateText(fromLanguageCode,toLanguageCode,sourceEdt.getText().toString());
+                }
+            }
+        });
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == REQUEST_PERMISSION_CODE){
+            if(resultCode == RESULT_OK && data != null){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                sourceEdt.setText(result.get(0));
+            }
+        }
+    }
+
+    private void translateText(int fromLanguageCode,int toLanguageCode,String source){
+            translatedTV.setText("Downloading Modal ...");
+        FirebaseTranslatorOptions options = new FirebaseTranslatorOptions.Builder()
+                .setSourceLanguage(fromLanguageCode)
+                .setTargetLanguage(toLanguageCode)
+                .build();
+
+        FirebaseTranslator translator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder().build();
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                translatedTV.setText("Translating ...");
+                translator.translate(source).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        translatedTV.setText(s);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(HomeActivity.this,"Fail To Translate: "+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(HomeActivity.this,"Fail To Download Language Modal "+e.getMessage(),Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -90,6 +160,10 @@ public class HomeActivity extends AppCompatActivity {
             case "Arabic":
                 languageCode = FirebaseTranslateLanguage.AR;
                 break;
+            case "Turkish":
+                languageCode = FirebaseTranslateLanguage.TR;
+                break;
+
             default:
                 languageCode = 0;
         }
