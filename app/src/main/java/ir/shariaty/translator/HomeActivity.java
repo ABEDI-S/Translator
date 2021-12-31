12c15c058fab66116f4878784cad61f09e41c389
ABEDI-S <abedi.b831@gmail.com>
2021-12-31 07:34:14 +0330

    Fifth Commit _ Home-JAVA Connect to Firebase Add Spinner Items

--- app/src/main/java/ir/shariaty/translator/HomeActivity.java
+++ app/src/main/java/ir/shariaty/translator/HomeActivity.java
@@ -3,12 +3,96 @@ package ir.shariaty.translator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Spinner fromSpinner,toSpinner;
+    private TextInputEditText sourceEdt;
+    private Button translateBtn;
+    private TextView translatedTV;
+
+    String[] toLanguages = {"English","Persian","Arabic"};
+    String[] fromLanguages = {"Persian","English","Arabic"};
+
+    private  static final int REQUEST_PERMISSION_CODE = 1;
+    int languageCode,fromLanguageCode,toLanguageCode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fromSpinner = findViewById(R.id.idFromSpinner);
+        toSpinner = findViewById(R.id.idToSpinner);
+        sourceEdt = findViewById(R.id.idEdtSource);
+        translateBtn = findViewById(R.id.btnTranslate);
+        translatedTV = findViewById(R.id.idTVtranslatedTV);
+
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                fromLanguageCode = getLanguageCode(fromLanguages[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        ArrayAdapter fromAdapter = new ArrayAdapter(this,R.layout.spinner_item,fromLanguages);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(fromAdapter);
+
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                toLanguageCode = getLanguageCode(toLanguages[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter toAdapter = new ArrayAdapter(this,R.layout.spinner_item,toLanguages);
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(toAdapter);
+
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translatedTV.setText("");
+            }
+        });
+    }
+
+    public int getLanguageCode(String language){
+        int languageCode = 0;
+        switch (language){
+            case "English":
+                languageCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Persian":
+                languageCode = FirebaseTranslateLanguage.FA;
+                break;
+            case "Arabic":
+                languageCode = FirebaseTranslateLanguage.AR;
+                break;
+            default:
+                languageCode = 0;
+        }
+        return languageCode;
     }
 }
